8a652e675666f50197697176d4aa7d2a7e7dee07
matac <e185742@ie.u-ryukyu.ac.jp>
2020-09-17 18:22:58 +0900

    faild adding to checked_list

--- app/controllers/scraps_controller.rb
+++ app/controllers/scraps_controller.rb
@@ -1,6 +1,7 @@
 class ScrapsController < ApplicationController
   def index
   @scraps = Scrap.all
+  @checked_list = []
 end
 
 def new
@@ -28,8 +29,18 @@ def destroy
     redirect_to scraps_path
 end
 
+# def destroy_all
+#   @check = 
+#   @deletes = []
+#   @deletes.push()
+
+# def delete_tasks
+#   params[:check].each do | di1, di2 |
+#     if di2 == "1"
+
+
   private
     def scrap_params
-      params.require(:scrap).permit(:title, :content)
+      params.require(:scrap).permit(:title, :content, :checked_list)
     end
 end
